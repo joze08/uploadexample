47074a77087c2dc190ec1e40cf4827c1e5698de1
joze08 <riosjunior270@gmail.com>
2021-05-12 10:26:43 -0300

    Heroku config yarnlock hello

--- start/routes.js
+++ start/routes.js
@@ -16,6 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
+Route.get('/', () => { return { ola: "Hello world!"} })
 Route.post('/register', 'AuthController.register');
 Route.post('/authenticate', 'AuthController.authenticate');
 
