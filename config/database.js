779fadd333675d07ea68a5f93fac437cb96ee5f7
joze08 <riosjunior270@gmail.com>
2021-05-14 10:25:48 -0300

    Sentindo que agora vai

--- config/database.js
+++ config/database.js
@@ -79,12 +79,8 @@ module.exports = {
       port: Env.get('DB_PORT', DATABASE_URL.port),
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)) 
-      /* host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis') */
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)),
+      ssl: { rejectUnauthorized: false },
     },
     /* debug: Env.get('DB_DEBUG', false) */
   }
